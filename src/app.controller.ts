911751e5ad6813245f342623a5b9d2b95a5b490d
RAKIBUL ISLAM <rakibul8226@gail.com>
2024-02-12 07:52:21 +0600

    validatinPipe added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,38 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { BookService } from './book.service';
+import { BookDto } from './dto/book.dto';
 
 @Controller('book')
 export class BookController {
   //   public bookService: BookService = new BookService();
   constructor(private bookService: BookService) {}
 
+  // @Post('/add')
+  // addBook(): string {
+  //   return this.bookService.addBook();
+  // }
+
   @Post('/add')
-  addBook(): string {
-    return this.bookService.addBook();
+  @UsePipes(new ValidationPipe())
+  async addBook(@Body() bookObj: BookDto): Promise<BookDto> {
+    // console.log(bookObj);
+    return this.bookService.addBook(bookObj);
   }
+
+  // @Post('/add')
+  // addBook(@Body(new ValidationPipe()) book: BookDto): string {
+  //   return this.bookService.addBook();
+  // }
 
   @Delete('/delete')
   deleteBook(): string {
@@ -26,8 +49,8 @@ export class BookController {
     return this.bookService.finalAllBooks();
   }
 
-  //   @Get('/findBookById/:id')
-  //   findBookById(@Param('id') id: string): Promise<string> {
-  //     return this.bookService.findBookById(id);
-  //   }
+  @Get('/findBookById/:id')
+  findBookById(@Param('id') id: string): Promise<string> {
+    return this.bookService.findBookById(id);
+  }
 }
--- src/book.service.ts
+++ src/book.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { BookDto } from './dto/book.dto';
 
 @Injectable()
 export class BookService {
-  addBook(): string {
-    return 'This will add book';
+  // addBook(): string {
+  //   return 'This will add a book';
+  // }
+
+  async addBook(bookObj: BookDto): Promise<BookDto> {
+    // console.log(bookObj);
+    return bookObj;
   }
 
   deleteBook(): string {
@@ -18,7 +24,7 @@ export class BookService {
     return 'This will find All Books';
   }
 
-  //   findBookById(id: string): Promise<string> {
-  //     return Promise.resolve(`Book with ID ${id}`);
-  //   }
+  findBookById(id: string): Promise<string> {
+    return Promise.resolve(`Book with ID ${id}`);
+  }
 }
